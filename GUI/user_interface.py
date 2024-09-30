6c7487a8c4f3b3979c7f414f7c70be45eab3cec3
Harilux <hariluxy@hotmail.com>
2024-09-30 15:18:14 +0200

    folder name change and tests added

--- GUI/user_interface.py
+++ GUI/user_interface.py
@@ -1,6 +1,6 @@
 import tkinter as tk
 from tkinter import filedialog, messagebox
-from Operations.processing_operations import process_verbs_and_save
+from operations.processing_operations import process_verbs_and_save
 
 class PolishVerbWizard:
     def __init__(self, root):
